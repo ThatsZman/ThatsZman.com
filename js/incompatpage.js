b4bd7d5bf84aabeef8bdd6904ea913b330a5da38
FusionLatte <zacharychaussee@gmail.com>
2020-07-29 22:01:52 -0400

    Added incompatible page alert.

---
+++ js/incompatpage.js
@@ -0,0 +1,19 @@
+function isIE() {
+    // IE 10 and IE 11
+    return /Trident\/|MSIE/.test(window.navigator.userAgent);
+}
+
+let showBrowserAlert = (function () {
+    if (document.querySelector('.unsupported-browser')) {
+        let d = document.getElementsByClassName('unsupported-browser');
+
+        is_IE11 = isIE11();
+
+        if (is_IE11) {
+            d[0].innerHTML = '<b>Unsupported Browser!</b> This website will offer limited functionality in this browser. We only support the recent versions of major browsers like Chrome, Firefox, Safari, and Edge.';
+            d[0].style.display = 'block';
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', showBrowserAlert);
